437ccab2a2d5d52a0535f2bdbc882b8fef100dcb
bintangyudhistira <162862180+bintangyudhistira@users.noreply.github.com>
2024-03-24 20:13:51 +0700

    Add files via upload

---
+++ Github Modul 2/guided2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int maks, a, i=1, lokasi;
+    cout << "masukkan panjang array: ";
+    cin >> a;
+    int array[a];
+    cout << "masukkan " << a << " angka\n";
+    for(i = 0; i < a; i++)
+    {
+        cout << "array ke-" << (i) << ": ";
+        cin >> array[i];
+    }
+    maks = array[0];
+    for (i=0;i<a;i++)
+    {
+        if (array[i] > maks)
+        {
+            maks = array[i];
+            lokasi = i;
+        }
+    }
+    cout << "nilai maksimum adalah " << maks << " berada di array ke " << lokasi << endl;
+
+    return 0;
+}
+
